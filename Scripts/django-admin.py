b9e3d40d4d4360d65f5fbc45793e7494babec87e
Assasingh7 <109212099+Assasingh7@users.noreply.github.com>
2024-06-13 15:14:01 +0530

    Initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\lenovo\Downloads\ecommerce-master\ecommerce-master\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
